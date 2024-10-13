f43794ff430e4df9223d8e955b3ff3a2e5026712
danielaNina <danielaninacastilo@gmail.com>
2024-10-13 11:37:54 -0400

    Implementacion header y footer

---
+++ app/Http/Controllers/perfilUsuarioController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class perfilUsuarioController extends Controller
+{
+    //
+    public function perfilUsuario()
+    {
+        return view('app-layout');
+
+    }
+}
---
+++ resources/views/components/Footer.blade.php
@@ -0,0 +1,49 @@
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="/css/Footer.css">
+    <footer>
+        <div class="contenedor">
+            <div class="sobre-mi">
+                <h2 class="titulos-footer">Quienes somos</h2>
+                <p>FG - Programador Web Full Stack es un canal de YouTube donde se subirá el paso a paso de todos los proyectos de programación realizados por Federico Geretto. Nació en diciembre de 2022 con el objetivo de alcanzar y llegar a más personas.</p>
+                <ul class="redes-sociales">
+                    <li><a href="#"><img class="img_fece"  src="{{ asset('images/fecebook.png') }}" alt="Facebook"></a></li>
+                    <li><a href="#"><img class="img_insta" src="{{ asset('images/instragram.png') }}" alt="Google"></a></li>
+                    <li><a href="#"><img class="img_google" src="{{ asset('images/google.png') }}" alt="Instagram"></li>  
+                   
+                </ul>
+            </div>
+
+            <div class="navegacion">
+                <h2 class="titulos-footer">Menú de navegación</h2>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="#">Catalogo</a></li>
+                    <li><a href="#">Contactanos</a></li>
+                </ul>
+            </div>
+
+            <div class="contactanos">
+                <h2 class="titulos-footer">Contáctanos</h2>
+                <ul class="informacion-contacto">
+                    <li>
+                        <span><i class="fa-solid fa-location-dot"></i></span>
+                        <p>Calle Evaristo Valle, Centro de Negocios "MERCATOS" N°127</p>
+                    </li>
+
+                    <li>
+                        <span><i class="fa-solid fa-phone"></i></span>
+                        <p>+54 3584 659871 <br> +54 3514 887634</p>
+                    </li>
+
+                    <li>
+                        <span><i class="fa-solid fa-envelope"></i></span>
+                        <p>glow_and_shine@gmail.com</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="copy">
+            Copyright © 2024 Glow & Shine
+        </div>
+</footer>
