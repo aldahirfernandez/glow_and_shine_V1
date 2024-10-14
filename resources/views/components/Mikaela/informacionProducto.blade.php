cd086c538e5984f588fde2b05da02297b2c4a9f6
MikaelaGisbert <karlamikaela.911@gmail.com>
2024-10-14 13:27:54 -0400

    Versión mejorada Fondo GRIS imagen

--- resources/views/components/Mikaela/informacionProducto.blade.php
+++ resources/views/components/Mikaela/informacionProducto.blade.php
@@ -2,7 +2,7 @@
 
 <div class="contenedor_descripcion_productos">
     <h4 class="texto_subtitulos_productos">Selecciona el tono de tu preferencia</h4>
-    <h4 class="texto_contenido_subtitulos">Alma</h4>
+    <h4 class="texto_contenido_subtitulos">{{ $producto->color }}</h4>
     <h4 class="texto_subtitulos_productos">Descripción</h4>
     <h4 class="texto_contenido_subtitulos">{{ $producto->descripcion }}</h4>
 </div>
