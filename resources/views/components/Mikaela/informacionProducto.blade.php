b3ff5241ba7393c7ff9c3aed07574f755ddbf967
MikaelaGisbert <karlamikaela.911@gmail.com>
2024-10-14 13:30:21 -0400

    Versión mejorada Fondo GRIS imagen MOD

--- resources/views/components/Mikaela/informacionProducto.blade.php
+++ resources/views/components/Mikaela/informacionProducto.blade.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="{{ asset('css/informacionProducto.css') }}">
 
 <div class="contenedor_descripcion_productos">
-    <h4 class="texto_subtitulos_productos">Selecciona el tono de tu preferencia</h4>
+    <h4 class="texto_subtitulos_productos">Tono disponible:</h4>
     <h4 class="texto_contenido_subtitulos">{{ $producto->color }}</h4>
     <h4 class="texto_subtitulos_productos">Descripción</h4>
     <h4 class="texto_contenido_subtitulos">{{ $producto->descripcion }}</h4>
