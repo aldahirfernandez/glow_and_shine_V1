661febc359fd588416fa03b0c84af0d4f00fd939
natalia <acuarionat2004@gmail.com>
2024-10-15 00:36:31 -0400

    antes de unir con home y producto

--- app/Http/Controllers/perfilUsuarioController.php
+++ app/Http/Controllers/perfilUsuarioController.php
@@ -21,12 +21,13 @@ class perfilUsuarioController extends Controller
             return redirect()->route('account.dashboard')->with('error', 'Usuario no encontrado');
         }
 
-        $saludo = 'Perfil de Empleado';
-        $mensajeB = 'Nos alegra que formes parte de nuestro 
-        equipo. Este es tu espacio personal, donde podrás gestionar 
-        tu información, acceder a recursos que te ayudaran a cumplir 
-        con tus tareas. ¡Estamos emocionados de tenerte con nosotros 
-        y esperamos que disfrutes de tu experiencia aquí!';
+        $saludo = 'Perfil de';
+        $mensajeB = 'Nos alegra que formes parte de nuestra 
+        comunidad. Este es tu espacio personal, 
+        donde podrás añadir tu información, 
+        explorar nuestros productos y más. 
+        ¡Disfruta de la experiencia!';
+
 
         return view('dashboard', compact('user', 'saludo', 'mensajeB'));
         dd(compact('user', 'saludo', 'mensajeB'));
